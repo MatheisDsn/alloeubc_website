e13b1de1e8890a9b7678bc00ea815c6d674d4d0e
MrMenphis <59884203+MatheisDsn@users.noreply.github.com>
2025-08-05 10:59:27 +0200

    Update create_superuser.py

--- alloeu_bc/mysite/create_superuser.py
+++ alloeu_bc/mysite/create_superuser.py
@@ -8,9 +8,11 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+admin_email = "matheisdasso@gmail.com"
+
 if not User.objects.filter(email=admin_email).exists():
     User.objects.create_superuser(
-        email="matheisdasso@gmail.com",
+        email=admin_email,
         password="Math200815!"
     )
     print("✅ Superuser créé")

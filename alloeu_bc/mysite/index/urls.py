9b2574ffa58b4783f21b586deac0d83ba9890ce9
MrMenphis <59884203+MatheisDsn@users.noreply.github.com>
2025-08-17 19:19:15 +0200

    Update urls.py

--- alloeu_bc/mysite/index/urls.py
+++ alloeu_bc/mysite/index/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("leclub/presentations/", views.presentation, name="presentations"),
     path("leclub/informations/", views.information, name="informations"),
+    path("leclub/partenaires/", views.partenaires, name="partenaires"),
     path("lesequipes/", views.lesequipes, name="lesequipes"),
     path("inscriptions/", views.inscriptions, name="inscriptions"),
     path("matchs/", views.matches, name="matches"),

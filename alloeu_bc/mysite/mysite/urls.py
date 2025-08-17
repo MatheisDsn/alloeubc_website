1da65a3787f349a17a46f685ad2288a1070f4448
MrMenphis <59884203+MatheisDsn@users.noreply.github.com>
2025-08-17 23:23:22 +0200

    Update urls.py

--- alloeu_bc/mysite/mysite/urls.py
+++ alloeu_bc/mysite/mysite/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.sitemaps.views import sitemap
+from index.sitemap import StaticViewSitemap, NewsSitemap
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -10,4 +12,5 @@ urlpatterns = [
     path("log/", include("accounts.urls", namespace='accounts')),
     path("__reload__/", include("django_browser_reload.urls")),
     path("admin/", admin.site.urls),
+    path("sitemap.xml", sitemap, {"sitemaps": {"static": StaticViewSitemap, "news": NewsSitemap}}, name="sitemap"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

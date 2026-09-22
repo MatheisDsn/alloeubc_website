b41a2b6a256453c723f8a57f6b271f3fdb1e150d
MrMenphis <59884203+MatheisDsn@users.noreply.github.com>
2026-09-22 18:30:00 +0200

    Update urls.py

--- alloeu_bc/mysite/mysite/urls.py
+++ alloeu_bc/mysite/mysite/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path("", include("index.urls", namespace='index')),
     path("actualites/", include("news.urls", namespace='news')),
     path("log/", include("accounts.urls", namespace='accounts')),
-        path("__reload__/", include("django_browser_reload.urls")),
     path("admin/", admin.site.urls),
     path("sitemap.xml", sitemap, {"sitemaps": {"static": StaticViewSitemap, "news": NewsSitemap}}, name="sitemap"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
